696710fd8f541980401cdd0a3c5f89702157f99a
FairusAnjay <jancokasuu06@gmail.com>
2022-10-04 18:54:47 +0700

    first commit

---
+++ plugins/donasi.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+//let handler = async(m, { conn, text, usedPrefix, command }) => {
+//let pp = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image')
+
+let str = `${global.wm}
+
+┌─「 Donasi • Pulsa 」
+│ • *Indosat:* 6285649064565
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Dana:* [${global.pdana}]
+│ • *Gopay:* [${global.pgopay}]
+│ • *Ovo:* [${global.povo}]
+│ • *Link Aja:* [${global.plinkaja}]
+❏────`
+let wibu = `https://telegra.ph/file/48e1fd3fa6dce7bf1242e.jpg` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://chat.whatsapp.com/G8Ofqsp0oIl19GPE5FI8Kk",
+    mediaType: "VIDEO",
+    description: "Join Grup Ngemtod", 
+    title: 'FaiRuzz MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig
+  }
+  } }) 
+          }
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
---
+++ plugins/info-script.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*Pingin Scnya?*
+Ketik .owner Dek.`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/fairuzzgapunyaduit",
+    mediaType: "VIDEO",
+    description: "Yahahahahahah Kontol", 
+    title: 'FaiRuzz MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+export default handler
